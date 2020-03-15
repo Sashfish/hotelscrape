b195781b702feebac6c786c5468cd3d2fecbcf0d
Sashfish <dormiii4@gmail.com>
2020-03-15 17:59:52 +0000

    Refactored curl execution, working on file saving

--- hotel_scraper.php
+++ hotel_scraper.php
@@ -1,87 +1,74 @@
 <?php
-$site_beginning = "https://www.expedia.co.uk/Hotel-Search?adults=2&destination=Aberdeen%2C%20Scotland%2C%20United%20Kingdom&endDate=";
-$range1 = array("2020-06-02","2020-06-01");
-$range2 = array("2020-06-19","2020-06-16");
-$range3 = array("2020-06-30","2020-06-25");
-$site_mid = "&latLong=57.123834%2C-2.156496&localDateFormat=d%2FM%2Fyyyy&regionId=278&searchPriorityOverride=213&selected=18926256&semdtl=&sort=PRICE_LOW_TO_HIGH&startDate=";
-$site_end = "&theme=&useRewards=true&userIntent";
+$GLOBALS['site_beginning'] = "https://www.expedia.co.uk/Hotel-Search?adults=2&destination=Aberdeen%2C%20Scotland%2C%20United%20Kingdom&endDate=";
+$GLOBALS['site_mid'] = "&latLong=57.123834%2C-2.156496&localDateFormat=d%2FM%2Fyyyy&regionId=278&searchPriorityOverride=213&selected=18926256&semdtl=&sort=PRICE_LOW_TO_HIGH&startDate=";
+$GLOBALS['site_end'] = "&theme=&useRewards=true&userIntent";
 
-$hotelname = "Marcliffe Hotel and Spa";
+$hotelname = "Marcliffe Hotel and Spa";//name tied to hotelid
 $file = getcwd() . "\scraped.csv";
 $current = "";
 
-$url1 = $site_beginning . $range1[0] . $site_mid . $range1[1] . $site_end;
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
-curl_setopt($curl, CURLOPT_URL, $url1);
-curl_setopt($curl, CURLOPT_REFERER, $url1);
+function scrape($in, $out){
+  $url = $GLOBALS['site_beginning'] . $out . $GLOBALS['site_mid'] . $in . $GLOBALS['site_end'];
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($curl, CURLOPT_HEADER, false);
+  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_REFERER, $url);
 
-$html = curl_exec($curl);
-curl_close($curl);
-echo "Scrape complete for 01 Jun - 02 Jun";
-echo "<br>";
-preg_match("/data-stid=\"content-hotel-lead-price\" aria-hidden=\"true\">(.*?\d)<\/span>/", $html, $match1);
-echo ($match1[1]);
-echo "<br>";
-
-$url2 = $site_beginning . $range2[0] . $site_mid . $range2[1] . $site_end;
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
-curl_setopt($curl, CURLOPT_URL, $url2);
-curl_setopt($curl, CURLOPT_REFERER, $url2);
-
-$html = curl_exec($curl);
-curl_close($curl);
-echo "Scrape complete for 16 Jun - 19 Jun";
-echo "<br>";
-preg_match("/data-stid=\"content-hotel-lead-price\" aria-hidden=\"true\">(.*?\d)<\/span>/", $html, $match2);
-echo ($match2[1]);
-echo "<br>";
-
-$url3 = $site_beginning . $range3[0] . $site_mid . $range3[1] . $site_end;
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
-curl_setopt($curl, CURLOPT_URL, $url3);
-curl_setopt($curl, CURLOPT_REFERER, $url3);
-
-$html = curl_exec($curl);
-curl_close($curl);
-echo "Scrape complete for 25 Jun - 30 Jun";
-echo "<br>";
-preg_match("/data-stid=\"content-hotel-lead-price\" aria-hidden=\"true\">(.*?\d)<\/span>/", $html, $match3);
-echo ($match3[1]);
-echo "<br>";
+  $html = curl_exec($curl);
+  curl_close($curl);
+  echo "Scrape complete for {$in} -- {$out}";
+  echo "<br>";
+  preg_match("/data-stid=\"content-hotel-lead-price\" aria-hidden=\"true\">(.*?\d)<\/span>/", $html, $match);
+  echo $match[1];
+  echo "<br>";
+  return array("in" => $in, "out" => $out, "price"=> $match[1]);
+}
 ?>
 
 <?php
+if(isset($_POST['button2'])){
+  scrape($_POST['in1'], $_POST['out1']);
+  scrape($_POST['in2'], $_POST['out2']);
+  scrape($_POST['in3'], $_POST['out3']);
+}
+
 if(isset($_POST['button1'])){
   if(!file_exists($file)){
     $current .= "\"Hotel Name\",\"Period\",\"Price\"\n";
-    $current .= "\"{$hotelname}\",\"{$range1[1]} - {$range1[0]}\",\"{$match1[1]}\"\n";
-    $current .= "\"{$hotelname}\",\"{$range2[1]} - {$range2[0]}\",\"{$match2[1]}\"\n";
-    $current .= "\"{$hotelname}\",\"{$range3[1]} - {$range3[0]}\",\"{$match3[1]}\"\n";
+    //$current .= "\"{$hotelname}\",\"{$range1[1]} - {$range1[0]}\",\"{$match1[1]}\"\n";
+    //$current .= "\"{$hotelname}\",\"{$range2[1]} - {$range2[0]}\",\"{$match2[1]}\"\n";
+    //$current .= "\"{$hotelname}\",\"{$range3[1]} - {$range3[0]}\",\"{$match3[1]}\"\n";
     file_put_contents($file, $current);
     $current = "";}
   else{
     $current = file_get_contents($file);
-    $current .= "\"{$hotelname}\",\"{$range1[1]} - {$range1[0]}\",\"{$match1[1]}\"\n";
-    $current .= "\"{$hotelname}\",\"{$range2[1]} - {$range2[0]}\",\"{$match2[1]}\"\n";
-    $current .= "\"{$hotelname}\",\"{$range3[1]} - {$range3[0]}\",\"{$match3[1]}\"\n";
+    //$current .= "\"{$hotelname}\",\"{$range1[1]} - {$range1[0]}\",\"{$match1[1]}\"\n";
+    //$current .= "\"{$hotelname}\",\"{$range2[1]} - {$range2[0]}\",\"{$match2[1]}\"\n";
+    //$current .= "\"{$hotelname}\",\"{$range3[1]} - {$range3[0]}\",\"{$match3[1]}\"\n";
     file_put_contents($file, $current);
     $current = "";}
 }
 ?>
+
+<p>Input the desired date ranges (format: YYYY-MM-DD)</p>
+<form method="post">
+  <label for="in1">1st In:</label>
+  <input type="text" id="in1" name="in1">
+  <label for="out1">1st Out:</label>
+  <input type="text" id="out1" name="out1"><br>
+  <label for="in2">2nd In:</label>
+  <input type="text" id="in2" name="in2">
+  <label for="out2">2nd Out:</label>
+  <input type="text" id="out2" name="out2"><br>
+  <label for="in3">3rd In:</label>
+  <input type="text" id="in3" name="in3">
+  <label for="out3">3rd Out:</label>
+  <input type="text" id="out3" name="out3"><br>
+  <input type="submit" name="button2" class ="button" value="Scrape">
+</form>
 <form method="post">
 <input type="submit" name="button1" class="button" value="Save results to csv" />
